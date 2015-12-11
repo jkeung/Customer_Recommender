170c738aa069165049cef96591abee517c59de8d
Jason Keung <jason.s.keung@gmail.com>
2015-12-11 14:08:32 -0500

    adding static

---
+++ static/datastore.js
@@ -0,0 +1,73 @@
+(function () {
+	var _rawData = [];
+	var _sortedData = [];
+	var _filteredData = [];
+
+	var avgRating = parseFloat($("#rating").text());
+	var numReviews = parseInt($("#review").text());
+
+	DataStore = window.DataStore = {
+		populateRawData: function (dataElement) {
+			_rawData.push(dataElement);
+		},
+
+
+		sortOnKeys: function () {
+
+		    var sortable = [];
+
+		    for(var i = 0; i < _rawData.length; i++) {
+
+		        sortable.push([_rawData[i], _rawData[i].avg_rating]);
+		    }
+
+			sortable.sort(function (b,a) { 
+				return a[1] - b[1];
+			});
+
+		    var tempData = [];
+
+		    for(var i = 0; i < sortable.length; i++) {
+		        tempData.push(sortable[i][0]);
+		    }
+
+		    _rawData = tempData;
+		},
+
+		populateFilteredData: function () {
+			_filteredData = [];
+
+			_rawData.forEach(function (data) {
+				if (data.avg_rating >= avgRating
+					&& data.num_reviews >= numReviews) {
+
+					_filteredData.push(data);
+				}
+			});
+		},
+
+		getFilteredData: function () {
+			return _filteredData;
+		},
+
+		getRawData: function () {
+			return _rawData;
+		},
+
+		getFilters: function () {
+			return {
+				avgRating: avgRating,
+				numReviews: numReviews
+			}
+		},
+
+		setAvgRating: function (rating) {
+			avgRating = rating;
+		},
+
+		setNumReviews: function (reviews) {
+			numReviews = reviews;
+		}
+
+	};
+}());
