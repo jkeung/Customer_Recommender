2e48deff7e2d82db463107bf5d525ddf3b37faaf
Jason Keung <jason.s.keung@gmail.com>
2015-12-12 15:23:16 -0500

    adding flask app

---
+++ bin/app.py
@@ -0,0 +1,23 @@
+import flask
+import numpy as np
+import pandas as pd
+
+#---------- URLS AND WEB PAGES -------------#
+
+# Initialize the app
+app = flask.Flask(__name__, template_folder='../templates', static_folder='../static')
+
+# Homepage
+@app.route("/")
+def viz_page():
+    """
+    Homepage: serve our visualization page, awesome.html
+    """
+
+    return flask.render_template('index.html')
+                                
+#--------- RUN WEB APP SERVER ------------#
+
+# Start the app server on port 80
+# (The default website port)
+app.run(host='0.0.0.0', port=5000)
